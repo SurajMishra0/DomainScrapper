5c93fac7398107f1b5b3ea5dae959e208d6d8260
Suraj Mishra <90396274+SurajMishra0@users.noreply.github.com>
2023-05-30 10:47:23 +0530

    Update Domain2IP.go
    In earlier version it was using string to check domain's respective ip, but now its taking input from file and saving it into a new file

--- Domain2IP.go
+++ Domain2IP.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"sync"
 )
 
@@ -19,7 +21,37 @@ func resolveDomain(domain string, wg *sync.WaitGroup, results chan string) {
 }
 
 func main() {
-	domains := []string{"example.com", "google.com", "github.com"}
+	inputFile := "input.txt"
+	outputFile := "output.txt"
+
+	// Open input file
+	input, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Failed to open input file: %s\n", err)
+		return
+	}
+	defer input.Close()
+
+	// Open output file
+	output, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Failed to create output file: %s\n", err)
+		return
+	}
+	defer output.Close()
+
+	scanner := bufio.NewScanner(input)
+
+	domains := []string{}
+	for scanner.Scan() {
+		domain := scanner.Text()
+		domains = append(domains, domain)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Failed to read input file: %s\n", err)
+		return
+	}
 
 	results := make(chan string)
 	var wg sync.WaitGroup
@@ -36,5 +68,8 @@ func main() {
 
 	for result := range results {
 		fmt.Println(result)
+		fmt.Fprintln(output, result)
 	}
+
+	fmt.Printf("Results saved to %s\n", outputFile)
 }
